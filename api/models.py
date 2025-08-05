77441b6d200c103fe2a06a81a414e75d5be23c53
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-06 00:36:28 +0600

    feat default status active done

--- api/models.py
+++ api/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+
+class BookStatus(models.TextChoices):
+    ACTIVE = 1, 'Active'
+    INACTIVE = 2, 'Inactive'
+    ARCHIVED = 3, 'Archived'
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
+    status = models.IntegerField(
+        choices=BookStatus.choices,
+        default=BookStatus.ACTIVE
+    )
