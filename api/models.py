c98a9be1f40905ca7c56aa24431befafdcebb9d9
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-08-08 03:32:20 +0600

    product models added

--- api/models.py
+++ api/models.py
@@ -13,3 +13,9 @@ class Book(models.Model):
         choices=BookStatus.choices,
         default=BookStatus.ACTIVE
     )
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
