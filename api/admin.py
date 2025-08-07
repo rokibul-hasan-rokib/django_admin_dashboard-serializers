04201bc2f1aa1225c38ba486296db56d207d1306
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-08-08 03:43:15 +0600

    admin added

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'is_active', 'created_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('name',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'price', 'is_active')
+        }),
+        ('Read-only Info', {
+            'fields': ('created_at',),
+            'classes': ('collapse',),
+        }),
+    )
